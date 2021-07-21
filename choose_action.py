c5d530351d3ff6d517eedfe2f2cebe5950dfd112
mliu05 <mliu05@mit.edu>
2021-07-21 12:27:31 -0700

    spfa (faster, yayy!)

--- choose_action.py
+++ choose_action.py
@@ -1,5 +1,7 @@
 from gym_sgw.envs.enums.Enums import Actions, Terrains, MapObjects
 import heapq
+from collections import deque
+
 def choose_action(observation):
     grid, status = observation
     print(status)
@@ -11,14 +13,44 @@ def choose_action(observation):
     dist = [[[[1000000 for _ in range(2)] for _ in range(4)] for _ in range(m)] for _ in range(n)]
     #prev_action_code = [[[Actions.none.value for _ in range(4)] for _ in range(m)] for _ in range(n)]
     prev_action = [[[[Actions.none for _ in range(2)] for _ in range(4)] for _ in range(m)] for _ in range(n)]
-    q = []
+    q = deque()
+    in_queue = [[[[False for _ in range(2)] for _ in range(4)] for _ in range(m)] for _ in range(n)]
     initiali = status['player_location'][0]
     initialj = status['player_location'][1]
     initialor = status['player_orientation'].value
     #heapq.heappush(q, (0, (initiali, initialj, initialor)))
-    dist[initiali][initialj][initialor][1 if MapObjects.injured in grid[initiali][initialj].objects else 0] = 0
-    can_break = True
+    initial_has_person = 1 if MapObjects.injured in grid[initiali][initialj].objects else 0
+    q.append((initiali, initialj, initialor, initial_has_person))
+    dist[initiali][initialj][initialor][initial_has_person] = 0
+    in_queue[initiali][initialj][initialor][initial_has_person] = True
+    q_count = 1
+    #can_break = True
     # bellman ford - could be optimized but 😶
+    while q_count > 0:
+        i, j, ori, has_person = q.popleft()
+        in_queue[i][j][ori][has_person] = False
+        q_count -= 1
+        # go forward
+        nexti = i + di[ori]
+        nextj = j + dj[ori]
+        if grid[nexti][nextj].terrain not in [Terrains.none, Terrains.out_of_bounds, Terrains.wall]:
+            next_has_person = 1 if MapObjects.injured in grid[nexti][nextj].objects or has_person else 0
+            if dist[i][j][ori][has_person] + 1 < dist[nexti][nextj][ori][next_has_person]:
+                dist[nexti][nextj][ori][next_has_person] = dist[i][j][ori][has_person] + 1
+                prev_action[nexti][nextj][ori][next_has_person] = Actions.step_forward
+                q.append((nexti, nextj, ori, next_has_person))
+                in_queue[nexti][nextj][ori][next_has_person] = True
+                q_count += 1
+        # turn left or right
+        for k in [-1, 1]:
+            nextori = (ori + k + 4) % 4
+            if dist[i][j][ori][has_person] + 1 < dist[i][j][nextori][has_person]:
+                dist[i][j][nextori][has_person] = dist[i][j][ori][has_person] + 1
+                prev_action[i][j][nextori][has_person] = Actions.turn_left if k == -1 else Actions.turn_right
+                q.append((i, j, nextori, has_person))
+                in_queue[i][j][nextori][has_person] = True
+                q_count += 1
+    '''
     for _ in range(n * m * 4 * 2):
         for i in range(n):
             for j in range(m):
@@ -44,6 +76,7 @@ def choose_action(observation):
                                 can_break = False
         if can_break:
             break
+    '''
     besti, bestj, bestori, bestdist = 0, 0, 0, 1000000
     for i in range(n):
         for j in range(m):
